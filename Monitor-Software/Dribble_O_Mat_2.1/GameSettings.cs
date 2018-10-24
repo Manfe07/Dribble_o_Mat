005bb12418e7b0927a34977c0032f03295eb5793
Manuel Fehren <37143951+Manfe07@users.noreply.github.com>
2018-10-24 18:39:17 +0200

    Adding int GameMode and String GameModeText()

--- Monitor-Software/Dribble_O_Mat_2.1/GameSettings.cs
+++ Monitor-Software/Dribble_O_Mat_2.1/GameSettings.cs
@@ -8,7 +8,29 @@ namespace Dribble_O_Mat_2._1
 {
     public class GameSettings
     {
+        public static int GameMode = 2;
         public int RoundTimeSeconds { get; set; } = 30;
         public int Sensitivity { get; set; } = 180;
+
+        public string GameModeText()
+        {
+            string text = "";
+            switch (GameMode)
+            {
+                case 0:
+                    text = "READY";
+                    break;
+                case 1:
+                    text = "GO!";
+                    break;
+                case 2:
+                    text = "FINISH";
+                    break;
+                default:
+                    text = "FINISH";
+                    break;
+            }
+            return text;
+        }
     }
 }
